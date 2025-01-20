aa0af2a22dbcb100579c829426885e87e9cba189
Vladyslav Shaposhnikov <vladyslav.shaposhnik@microsoft.wsei.edu.pl>
2025-01-20 20:51:40 +0100

    init commit

---
+++ MoviesApi/Controllers/MoviesController.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Models;
+using MoviesApi.Models.Movies;
+
+namespace MoviesApi.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesContext _context;
+
+        public MoviesController(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movie
+        public IActionResult Index(int page = 1, int size = 10)
+        {
+            return View( PagingListAsync<Movie>.Create(
+                (p, s) => 
+                    _context.Movies
+                        .OrderBy(b => b.Title)
+                        .Skip((p - 1) * s)
+                        .Take(s)
+                        .AsAsyncEnumerable(),
+                _context.Movies.Count(),
+                page,
+                size));
+        }
+
+        // GET: Movie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movie/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("Title, Overview, ReleaseDate, CompanyId")]
+            MovieModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int newId = _context.Movies.Any()
+                    ? _context.Movies.Max(m => m.MovieId) + 1
+                    : 1;
+                
+                var newMovie = new Movie
+                {
+                    MovieId = newId,
+                    Title = model.Title,
+                    Overview = model.Overview,
+                    ReleaseDate = model.ReleaseDate
+                };
+                _context.Movies.Add(newMovie);
+                await _context.SaveChangesAsync(); 
+                
+                if (!_context.ProductionCompanies.Any(c => c.CompanyId == model.CompanyId))
+                {
+                    throw new Exception($"Production Company with ID {model.CompanyId} does not exist.");
+                }
+                
+                var newMovieCompany = new MovieCompany
+                {
+                    MovieId = newMovie.MovieId,   
+                    CompanyId = model.CompanyId  
+                };
+                
+                _context.MovieCompanies.Add(newMovieCompany);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+        
+        // GET: Movie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+
+        // POST: Movie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MovieId,Title,Budget,Homepage,Overview,Popularity,ReleaseDate,Revenue,Runtime,MovieStatus,Tagline,VoteAverage,VoteCount")] Movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.MovieId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movie/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.MovieId == id);
+        }
+    }
+}
---
+++ MoviesApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MoviesApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
